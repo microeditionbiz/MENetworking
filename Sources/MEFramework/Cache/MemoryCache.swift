7a32a0b6b1da1deea85c6221e39084cb005182ef
Pablo Romero <pablo.romero@glovoapp.com>
2022-01-22 18:35:15 +0100

    Added initial version of ComposableCache

---
+++ Sources/MEFramework/Cache/MemoryCache.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pablo Ezequiel Romero Giovannoni on 04/01/2022.
+//
+
+import Foundation
+
+final public class MemoryCache<Value>: Cache {
+    private class ObjectWrapper: NSObject {
+        let _value: Value
+
+        init(value: Value) {
+            self._value = value
+        }
+    }
+
+    private let cache = NSCache<NSString, ObjectWrapper>()
+
+    public func set(_ value: Value, for key: String) {
+        cache.setObject(.init(value: value), forKey: key as NSString)
+    }
+
+    public func get(for key: String) -> Value? {
+        cache.object(forKey: key as NSString)?._value
+    }
+}
